925f7fde636c4a44a82de39979a507a82a80dce9
Juampa Rodríguez <32236059+und3sc0n0c1d0@users.noreply.github.com>
2020-11-13 18:39:36 +0100

    Update UserEnumJira.sh

--- UserEnumJira.sh
+++ UserEnumJira.sh
@@ -6,8 +6,15 @@ echo "        ║ ║└─┐├┤ ├┬┘║╣ ││││ ││││"
 echo "        ╚═╝└─┘└─┘┴└─╚═╝┘└┘└─┘┴ ┴"
 echo "      Escrito por Juampa Rodríguez      "
 echo "----------------------------------------"
-echo ""
 
-curl -s "https://"$1"/activity" | grep -n "<name>" | cut -d ">" -f3-8 | sort | uniq | sed 's/<name>/ Nombre: /g' | sed 's/<\/name><email>/\n Email: /g' | sed 's/<\/email><uri>https:\/\/'$1'\/secure\/ViewProfile.jspa?name=/\n Username: /g' | sed 's/<\/uri/\n/'
+output=$(curl -s "https://"$1"/activity" | sed 's/<name>/\n Nombre: /g' | sed 's/<\/uri>/\n/g' | grep "Nombre:" | sort | uniq)
 
-echo "----------------------------------------"
+if [ -z $1 ]
+    then echo -e "\n\tUso: ./UserEnumJira.sh TARGET\n"
+    exit 0
+    fi
+    if [[ $output == *"?name="* ]];
+        then echo $output | sed 's/name=/\n Username: /g' | sed 's/<\/name><email>/\n Email: /g' | sed 's/<\/email><uri>/\n/g' | grep -v http | sed 's/Nombre:/\n\n Nombre:/g'
+        else echo $output | sed 's/display\/~/\n Username: /g' | sed 's/<\/name><email>/\n Email: /g' | sed 's/<\/email><uri>/\n/g' | grep -v http | sed 's/Nombre:/\n\n Nombre:/g'
+        fi
+echo -e "\n----------------------------------------"
